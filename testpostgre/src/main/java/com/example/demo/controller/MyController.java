2c73bdb597c18fb72b8fe8fae5bbd642980d0b17
Suraj <surajkr9845@gmail.com>
2026-06-29 13:53:35 +0530

    Added demoh2 and testpostgre projects

---
+++ testpostgre/src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,75 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.Repo.MyRepo;
+import com.example.demo.entity.Employee;
+
+@RestController
+public class MyController {
+
+    @Autowired
+    private MyRepo repository;
+
+    // GET ALL
+    @GetMapping("/getAllEmployee")
+    public List<Employee> getAllEmployee() {
+        return repository.findAll();
+    }
+
+    // GET BY ID
+    @GetMapping("/getAllEmployee/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+        Optional<Employee> emp = repository.findById(id);
+        return emp.orElse(null);
+    }
+
+    // ADD
+    @PostMapping("/addEmployee")
+    public String addEmployee(@RequestBody Employee employee) {
+        repository.save(employee);
+        return "Employee added successfully";
+    }
+
+    // UPDATE
+    @PutMapping("/updateEmployee/{id}")
+    public String updateEmployee(@PathVariable int id,
+                                 @RequestBody Employee updatedEmployee) {
+
+        Optional<Employee> optional = repository.findById(id);
+
+        if (optional.isPresent()) {
+            Employee emp = optional.get();
+            emp.setName(updatedEmployee.getName());
+            emp.setSalary(updatedEmployee.getSalary());
+
+            repository.save(emp);
+            return "Employee updated successfully";
+        }
+
+        return "Employee not found";
+    }
+
+    // DELETE BY ID
+    @DeleteMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return "Employee deleted successfully";
+        }
+
+        return "Employee not found";
+    }
+
+    // DELETE ALL
+    @DeleteMapping("/deleteAllEmployee")
+    public String deleteAllEmployee() {
+        repository.deleteAll();
+        return "All employees deleted";
+    }
+}
